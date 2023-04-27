57f804dda28ea5aa412fcee4ee1c7f68a42796d9
Sam Hollenbeck <sam@hollenbecks.com>
2023-04-27 15:15:10 -0500

    cleaned up code (working HistGradientBoostingClassifier model)

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def main():
     x = load_data("data/train_x.csv")
     y = load_data("data/train_y.csv")
 
-    train_x, train_y, test_x, test_y = split_data(x, y)
+    train_x, train_y, val_x, val_y = split_data(x, y)
 
     ###### Your Code Here #######
     # Add anything you want here
@@ -36,51 +36,10 @@ def main():
     ###### Your Code Here #######
     # Add anything you want here
 
-    import numpy as np
-
-    processed_x_train = processed_x_train.rename(columns={'ethnicity_Native American': 'ethnicity_Native_American'})
-    processed_x_test = processed_x_test.assign(ethnicity_Native_American=np.zeros(len(processed_x_test)))
-
-    processed_x_train = processed_x_train.sort_index(axis=1)
-    processed_x_test = processed_x_test.sort_index(axis=1)
-
-    x_train_agg = processed_x_train.groupby('patientunitstayid', as_index=False).agg(np.nanmean)
-    x_train_agg = x_train_agg.reset_index(drop=True)
-    # x_train_agg.to_csv("testinglol.csv")
-
-    x_test_agg = processed_x_test.groupby('patientunitstayid', as_index=False).agg(np.nanmean)
-    x_test_agg = x_test_agg.reset_index(drop=True)
-    # x_test_agg.to_csv("testinglol.csv")
-
-    train_y = train_y.sort_values('patientunitstayid')
-    train_y = train_y.drop(train_y.columns[0], axis=1)
-    test_y = test_y.sort_values('patientunitstayid')
-    test_y = test_y.drop(test_y.columns[0], axis=1)
-
-    from sklearn.ensemble import HistGradientBoostingClassifier
-    model = HistGradientBoostingClassifier()
-
-    model.fit(X=x_train_agg, y=train_y.values.ravel())
-
-    from sklearn.metrics import roc_auc_score
-
-    # class_probs = model.predict_proba(x_test_agg)
-    y_pred_proba = model.predict_proba(x_test_agg)
-    print(y_pred_proba)
-    df_combined = pd.concat([x_test_agg['patientunitstayid'], pd.Series(y_pred_proba[:,1])], axis=1)
-
-    cols = df_combined.columns.tolist()
-    cols[1] = 'hospitaldischargestatus'
-    df_combined.columns = cols
-
-    df_combined.to_csv("submission.csv", index=False, float_format='%.14f')
-
-
-
+    processed_x_val = preprocess_x(val_x)
 
     # auc_roc = roc_auc_score(test_y, y_pred_proba[:,1])
     # print("Taadaa: ", auc_roc)
-    exit()
 
     # # x_train_agg = arr[np.argsort(arr[:, 1])]
 
@@ -88,10 +47,12 @@ def main():
 
     model = Model(args)  # you can add arguments as needed
     model.fit(processed_x_train, train_y)
-    x = load_data("test_x.csv")
+    x = load_data("data/test_x.csv")
 
     ###### Your Code Here #######
     # Add anything you want here
+
+    model.score(processed_x_val, val_y)
 
     ############################
 
--- model.py
+++ model.py
@@ -1,20 +1,135 @@
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import HistGradientBoostingClassifier
+
+from sklearn.metrics import roc_auc_score
+
+import pandas as pd
+import numpy as np
+
 class Model():
     def __init__(self, args):
         ############################ Your Code Here ############################
         # Initialize your model in this space
         # You can add arguements to the initialization as needed
-        m = 1
+        self.scaler = None
+        self.normalize_features = None
+        self.columns = None
+
+        self.encoded_columns = ['ethnicity', 'cellattributevalue']
+        self.normalized_columns = ['age', 'admissionheight', 'admissionweight', 'bmi', 'offset', 'unitvisitnumber']
+        self.pivot_columns = ['nursingchartvalue', 'labresult']
+
+        self.model = None
         ########################################################################
 
-    # def fit(self, x_train, y_train, x_val=None, y_val=None):
-    #     ############################ Your Code Here ############################
-    #     # Fit your model to the training data here
+    def fit(self, x_train, y_train, x_val=None, y_val=None):
+        ############################ Your Code Here ############################
+        # Fit your model to the training data here
 
-    #     ########################################################################
+        self.columns = x_train.columns
 
-    # def predict_proba(self, x):
-    #     ############################ Your Code Here ############################
-    #     # Predict the probability of in-hospital mortaility for each x
+        x_train = self.train_normalize(x_train)
+        x_train = self.aggregate(x_train)
 
-    #     ########################################################################
-    #     return preds
+        x_train = x_train.sort_values('patientunitstayid')
+        y_train = y_train.sort_values('patientunitstayid')
+
+        x_train = x_train.drop('patientunitstayid', axis=1)
+
+        self.model = HistGradientBoostingClassifier()
+        self.model.fit(X=x_train, y=y_train['hospitaldischargestatus'])
+
+        ########################################################################
+
+    def predict_proba(self, x):
+        ############################ Your Code Here ############################
+        # Predict the probability of in-hospital mortaility for each x
+
+        x = self.fix_columns(self.columns, x)
+        x = self.test_normalize(x)
+        x = self.aggregate(x)
+
+        ids = x['patientunitstayid']
+        x = x.drop('patientunitstayid', axis=1)
+
+        preds = self.model.predict_proba(x)
+
+        df_combined = pd.concat([ids, pd.Series(preds[:,1])], axis=1)
+
+        cols = df_combined.columns.tolist()
+        cols[1] = 'hospitaldischargestatus'
+        df_combined.columns = cols
+
+        df_combined.to_csv("submission.csv", index=False, float_format='%.14f')
+
+        ########################################################################
+        return preds
+
+    def score(self, x_val, y_val):
+        x_val = self.fix_columns(self.columns, x_val)
+        x_val = self.test_normalize(x_val)
+        x_val = self.aggregate(x_val)
+
+        x_val = x_val.sort_values('patientunitstayid')
+        y_val = y_val.sort_values('patientunitstayid')
+
+        x_val.to_csv("x_val.csv", index=False)
+
+        x_val = x_val.drop('patientunitstayid', axis=1)
+
+        pred_proba = self.model.predict_proba(x_val)
+
+        score = roc_auc_score(y_val['hospitaldischargestatus'], pred_proba[:,1])
+        print('ROC-AOC:', score)
+
+    def train_normalize(self, df):
+        # pivot creates new columns, these columns need to be normalized too
+        for c in df.columns:
+            if "nursingchartvalue" in c or "labresult" in c:
+                self.normalized_columns.append(c)
+
+        # normalizing
+        from sklearn.preprocessing import StandardScaler
+        self.scaler = StandardScaler()
+        normalized_data = self.scaler.fit_transform(df[self.normalized_columns])
+        normalized_df = pd.DataFrame(normalized_data, columns=self.normalized_columns)
+        df[self.normalized_columns] = normalized_df
+
+        return df
+
+    def test_normalize(self, df):
+        # normalizing
+        normalized_data = self.scaler.transform(df[self.normalized_columns])
+        normalized_df = pd.DataFrame(normalized_data, columns=self.normalized_columns)
+        df[self.normalized_columns] = normalized_df
+
+        return df
+
+    def fix_columns(self, train_x_columns, test_df):
+        if not set(train_x_columns) == set(test_df.columns):
+            missing_columns = set(train_x_columns) - set(test_df.columns)
+
+            for column in missing_columns:
+                # if self.is_column_encoded(column):
+                #     test_df[column] = 0
+                # else:
+                #     test_df[column] = np.nan
+
+                test_df[column] = np.nan
+
+            additional_columns = set(test_df.columns) - set(train_x_columns)
+            test_df = test_df.drop(columns=additional_columns)
+            test_df = test_df.sort_index(axis=1)
+
+        return test_df
+
+    def is_column_encoded(self, column_name):
+        for c in self.encoded_columns:
+            if column_name in c:
+                return True
+        return False
+
+    def aggregate(self, df):
+        df = df.groupby('patientunitstayid', as_index=False).agg(np.nanmean)
+        df = df.reset_index(drop=True)
+        return df
